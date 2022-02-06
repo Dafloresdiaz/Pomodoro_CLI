c0edaf9d91a265e969e69f47cb2ce71798942d55
dafloresdiaz <dafloresdiaz@gmail.com>
2022-02-06 14:13:00 -0600

    Create the intial commit, created a swith case to handle the instructions

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// Main function to start, pause or stop the pomodoro.
+
+	var instruction = flag.String(
+		"instruction",
+		"start",
+		"Instruction to start or stop a pomodoro",
+	)
+	flag.Parse()
+	var instruction_value = *instruction
+	var no_valid_option = errors.New(
+		"No instruction provided, please use start or stop")
+
+	if instruction_value != "" {
+		instruction_value = strings.ToLower(instruction_value)
+	}
+
+	switch instruction_value {
+	case "start":
+		fmt.Println("Start Pomodoro")
+	case "stop":
+		fmt.Println("Stop Pomodoro")
+	default:
+		fmt.Print(no_valid_option)
+	}
+}
