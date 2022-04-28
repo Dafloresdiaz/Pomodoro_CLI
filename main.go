0c689f56cf91828c72f8833ccbf73a1611ef38de
Daniel Flores Diaz <daniel.flores@Daniels-MacBook-Pro.local>
2022-04-27 20:10:14 -0500

    Use cobra cli to create an app using this library

--- main.go
+++ main.go
@@ -1,35 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"errors"
-	"flag"
-	"fmt"
-	"strings"
-)
+import "Pomodoro_CLI/cmd"
 
 func main() {
-	// Main function to start, pause or stop the pomodoro.
-
-	var instruction = flag.String(
-		"instruction",
-		"start",
-		"Instruction to start or stop a pomodoro",
-	)
-	flag.Parse()
-	var instruction_value = *instruction
-	var no_valid_option = errors.New(
-		"No instruction provided, please use start or stop")
-
-	if instruction_value != " " {
-		instruction_value = strings.ToLower(instruction_value)
-	}
-
-	switch instruction_value {
-	case "start":
-		fmt.Println("Start Pomodoro")
-	case "stop":
-		fmt.Println("Stop Pomodoro")
-	default:
-		fmt.Print(no_valid_option)
-	}
+	cmd.Execute()
 }
